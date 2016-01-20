56cb250ae697da5f5d7f353dff1893976b2dd7c6
QuakersOat <quakersoat@gmail.com>
2016-01-20 08:47:41 -0600

    Let's get back to it!
    Named each area.

--- main.js
+++ main.js
@@ -1,9 +1,97 @@
-document.getElementById("1").addEventListener("click", one);
-document.getElementById("2").addEventListener("click", two);
+document.getElementById("1").addEventListener("mouseover", one);
+document.getElementById("2").addEventListener("mouseover", two);
+document.getElementById("3").addEventListener("mouseover", three);
+document.getElementById("4").addEventListener("mouseover", four);
+document.getElementById("5").addEventListener("mouseover", five);
+document.getElementById("6").addEventListener("mouseover", six);
+document.getElementById("7").addEventListener("mouseover", seven);
+document.getElementById("8").addEventListener("mouseover", eight);
+document.getElementById("9").addEventListener("mouseover", nine);
+document.getElementById("10").addEventListener("mouseover", ten);
+document.getElementById("11").addEventListener("mouseover", eleven);
+document.getElementById("12").addEventListener("mouseover", twelve);
+document.getElementById("13").addEventListener("mouseover", thirteen);
+document.getElementById("14").addEventListener("mouseover", fourteen);
+document.getElementById("15").addEventListener("mouseover", fifteen);
+document.getElementById("16").addEventListener("mouseover", sixteen);
+document.getElementById("17").addEventListener("mouseover", seventeen);
+document.getElementById("18").addEventListener("mouseover", eighteen);
+document.getElementById("19").addEventListener("mouseover", nineteen);
+document.getElementById("20").addEventListener("mouseover", twenty);
+document.getElementById("21").addEventListener("mouseover", twentyone);
+document.getElementById("22").addEventListener("mouseover", twentytwo);
+document.getElementById("23").addEventListener("mouseover", twentythree);
+document.getElementById("24").addEventListener("mouseover", twentyfour);
 
 function one() {
-    document.getElementById("demo").innerHTML = Date();
+    document.getElementById("info").innerHTML = "Nokuy";
 }
 function two() {
-    document.getElementById("demo").innerHTML = prompt("Do you got a big load?");
+    document.getElementById("info").innerHTML = "Tsewhtron";
+}
+function three() {
+    document.getElementById("info").innerHTML = "Tuvanun";
+}
+function four() {
+    document.getElementById("info").innerHTML = "Adanac Ruof";
+}
+function five() {
+    document.getElementById("info").innerHTML = "Airotciv";
+}
+function six() {
+    document.getElementById("info").innerHTML = "Sknab";
+}
+function seven() {
+    document.getElementById("info").innerHTML = "Adanac Neves";
+}
+function eight() {
+    document.getElementById("info").innerHTML = "Niffab";
+}
+function nine() {
+    document.getElementById("info").innerHTML = "Trela";
+}
+function ten() {
+    document.getElementById("info").innerHTML = "Etuloser";
+}
+function eleven() {
+    document.getElementById("info").innerHTML = "Eremselle";
+}
+function twelve() {
+    document.getElementById("info").innerHTML = "Aibmuloc Hsitirb";
+}
+function thirteen() {
+    document.getElementById("info").innerHTML = "Atrebla";
+}
+function fourteen() {
+    document.getElementById("info").innerHTML = "Nawehctaksas";
+}
+function fifteen() {
+    document.getElementById("info").innerHTML = "Abotinam";
+}
+function sixteen() {
+    document.getElementById("info").innerHTML = "Oiratno";
+}
+function seventeen() {
+    document.getElementById("info").innerHTML = "Cebeuq";
+}
+function eighteen() {
+    document.getElementById("info").innerHTML = "Sllaf Llihcruhc";
+}
+function nineteen() {
+    document.getElementById("info").innerHTML = "Snhojts";
+}
+function twenty() {
+    document.getElementById("info").innerHTML = "Kciwsnurb";
+}
+function twentyone() {
+    document.getElementById("info").innerHTML = "I.E.P.";
+}
+function twentytwo() {
+    document.getElementById("info").innerHTML = "Aitocs Avon";
+}
+function twentythree() {
+    document.getElementById("info").innerHTML = "Adanac Eerht Ytnewt";
+}
+function twentyfour() {
+    document.getElementById("info").innerHTML = "Adanac Ruof Ytnewt";
 }
